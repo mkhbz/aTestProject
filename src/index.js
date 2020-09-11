52033b9f14bcb5d49d97d8d7a7d7cc4c19e59eb0
3471433975@qq.com <3471433975@qq.com>
2020-09-11 15:58:14 +0800

    提交一般测试版本

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("a")
+import { AEXECEL } from { aexece }
+var myExecel = new AEXECEL.AExecel('fileName');
+// myExecel.CreateExcel();
+const title = ['标题1', '标题2', '标题3', '标题4']
+let data = new Array(1000000);
+data.fill({ 'name1': 'value1', 'name2': 'value2', 'name3': 'value3', 'name4': 'value4' })
+// const data = [{ 'name1': 'value1', 'name2': 'value2', 'name3': 'value3', 'name4': 'value4' }]
+myExecel.createLargerCsv(title, data)
